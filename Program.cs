95d0e85f4d81c6334c97dc69217528bde26b6ce2
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-09-05 12:37:25 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+//Declaração do Array
+int[] arrayInteiros = new int[3];
+
+//Preenchendo o Array
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 128;
+
+//Percorrendo os índices e mostando em Tela
+Console.WriteLine("FOR");
+for(int i = 0; i < arrayInteiros.Length; i++) 
+{
+    Console.WriteLine($"Posição Nº {i} - Valor = {arrayInteiros[i]}");
+}
+
+//Percorrendo os índices com FORECH
+Console.WriteLine("FOREACH");
+int contador = 0;
+foreach(int i in arrayInteiros)
+{
+    Console.WriteLine($"Posição Nº {contador} - Valor = {i} ");
+    contador++;
+}
